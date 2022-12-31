d0e2101059ea6d09b8059c1be71eebc1af0acfb6
Shivika Tyagi <iamshivikatyagi@gmail.com>
2022-12-31 13:56:59 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ const app =express()
 const port = process.env.PORT||3000
 app.use(express.json())
 app.use(noteRouter)
-app.get("/", function(req,res){
-    res.send(" This is the HOME PAGE");
-});
+
 app.listen(port,()=>{
     console.log('Server is up on port ' + port)
 })
